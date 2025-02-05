9f2adc82becfad9c202e3de148b228c9d3f7f466
James <jamessoh11@gmail.com>
2025-02-05 22:33:33 +0800

    Initial commit

---
+++ src/test/java/com/example/bank/MainTest.java
@@ -0,0 +1,12 @@
+package com.example.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testHelloWorld() {
+        String message = "Hello, Maven!";
+        assertEquals("Hello, Maven!", message);
+    }
+}
