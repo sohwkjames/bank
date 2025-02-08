11bd551e8c79ca2e5cce2b0d740df200066bdfe1
James <jamessoh11@gmail.com>
2025-02-08 22:02:37 +0800

    add bankService

--- src/main/java/com/example/bank/service/BankService.java
+++ src/main/java/com/example/bank/service/BankService.java
@@ -3,24 +3,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.bank.model.Account;
+import com.example.bank.model.Transaction;
 
 public class BankService {
 	private List<Account> accounts = new ArrayList<>();
 //	private InterestRuleService interestService;
 	
 	public BankService(List<Account> accounts) {
-		super();
 		this.accounts = accounts;
 	}
 	
 	public BankService() {	
 	}
-
-	boolean addAccount(Account acc) {
-		return true;
-	}
 	
-	Account getAccountById(String accountId) {
+	public Account getAccountById(String accountId) {
+		for (Account account : accounts) {
+			if (account.getAccountId().equals(accountId)) {
+				return account;
+			}
+		}
+		
 		return null;
+	}
+
+	public Account addTransactionToAccount(String accountId, Transaction transaction) throws Exception {
+		// Fetch the account
+		Account account;
+		account = getAccountById(accountId);
+		if (account == null) {
+			account = new Account(accountId);
+			accounts.add(account);
+		}
+
+		account.addTransaction(transaction);
+		
+		return account;
 	}
 }
